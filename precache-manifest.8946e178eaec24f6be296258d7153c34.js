5faf2eb93a35e561ce8263eb8b64e43fc1027a97
abhishekgiram <abhishek.giram11@gmail.com>
2020-03-12 18:12:00 +0530

    Updates

---
+++ precache-manifest.8946e178eaec24f6be296258d7153c34.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "897a3ca1e038c184daa21009ecca16fa",
+    "url": "/clicker/index.html"
+  },
+  {
+    "revision": "a5ca50bbce70d89c5d21",
+    "url": "/clicker/static/css/main.54d83e3c.chunk.css"
+  },
+  {
+    "revision": "02f0817f51911a13a272",
+    "url": "/clicker/static/js/2.dc2c6dd7.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/clicker/static/js/2.dc2c6dd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5ca50bbce70d89c5d21",
+    "url": "/clicker/static/js/main.2d64b719.chunk.js"
+  },
+  {
+    "revision": "e887fb5ef24347969828",
+    "url": "/clicker/static/js/runtime-main.1d87b309.js"
+  }
+]);
